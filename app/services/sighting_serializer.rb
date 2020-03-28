8b82b47500c30787c0455774cca0f7135e0b671e
carolgreene <carolgreene3@gmail.com>
2020-03-28 07:04:19 -0700

    finished lab

---
+++ app/services/sighting_serializer.rb
@@ -0,0 +1,30 @@
+class SightingSerializer
+
+  def initialize(sighting_object)
+    @sighting = sighting_object
+  end
+
+  #def to_serialized_json 
+    #@sighting.to_json(:include => {
+    #  :bird => {:only => [:name, :species]},
+    #  :location => {:only => [:latitude, :longitude]}
+   # }, :except => [:updated_at])
+  #end
+
+  #could also make an options variable hash to show what you want included
+
+  def to_serialized_json 
+    options = {
+      include: {
+        bird: {
+          only: [:name, :species]
+        },
+        location: {
+          only: [:latitude, :longitude]
+        }
+      },
+      except: [:updated_at]
+    }
+    @sighting.to_json(options)
+  end
+end
